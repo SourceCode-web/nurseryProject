bcd9a3929b40b96e8f1d8fd9eeca7edd9810bf72
SourceCode-web <54756127+SourceCode-web@users.noreply.github.com>
2025-08-05 17:47:48 -0500

    Update User_Interface.h
    quick notes on user interface

--- User_Interface.h
+++ User_Interface.h
@@ -6,7 +6,20 @@ class User_Interface
         
         -> customer: can see a list of the inventory of all the plant species 
 
-        -> staff: will ask wheth
+        -> staff: will ask which option the user wants. 
+                1) Look at inventory
+                2) Add Sale
+                3) Add Species
+                4) Partially remove species?
+
+        -> Admin: Will also ask which option the user wants.
+                1) Look at inventory
+                2) Add Sale
+                3) Add Species
+                4) remove species (without a sale)
+                5) Generate Report
+                6) add/remove staff
+                7) edit Staff
     */
 };
 
